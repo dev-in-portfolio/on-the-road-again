d527946cc60e2d72275f468092c521f2644ca5be
dev-in-portfolio <devin.dev.portfolio@gmail.com>
2026-08-17 12:11:08 -0400

    bump Android app to 1.0.2

--- src/app-release.ts
+++ src/app-release.ts
@@ -1,5 +1,4 @@
 export const OTRA_PACKAGE_ID = 'com.darkstar.otra';
-export const OTRA_VERSION = '1.0.1';
-export const OTRA_BUILD = 10001;
+export const OTRA_VERSION = '1.0.2';
+export const OTRA_BUILD = 10002;
 export const OTRA_CHANNEL = 'stable' as const;
-
