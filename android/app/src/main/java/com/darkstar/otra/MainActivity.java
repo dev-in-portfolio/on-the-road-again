e0e6de2ddfc0aacba613593b7ad716cf43752472
dev-in-portfolio <devin.dev.portfolio@gmail.com>
2026-08-17 14:22:28 -0400

    measure Android bottom navigation inset

--- android/app/src/main/java/com/darkstar/otra/MainActivity.java
+++ android/app/src/main/java/com/darkstar/otra/MainActivity.java
@@ -1,6 +1,11 @@
 package com.darkstar.otra;
 
+import android.view.View;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
+import java.util.Locale;
 
 public class MainActivity extends BridgeActivity {
     @Override
@@ -8,5 +13,24 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(OtraUpdaterPlugin.class);
         registerPlugin(OtraNavigationPlugin.class);
         super.onCreate(savedInstanceState);
+        installSystemInsetBridge();
+    }
+
+    private void installSystemInsetBridge() {
+        if (getBridge() == null || getBridge().getWebView() == null) return;
+        View webView = getBridge().getWebView();
+        ViewCompat.setOnApplyWindowInsetsListener(webView, (view, windowInsets) -> {
+            Insets navigationBars = windowInsets.getInsets(WindowInsetsCompat.Type.navigationBars());
+            float density = getResources().getDisplayMetrics().density;
+            float bottomDp = density > 0 ? navigationBars.bottom / density : navigationBars.bottom;
+            String cssValue = String.format(Locale.US, "%.2fpx", bottomDp);
+            view.post(() -> {
+                if (getBridge() == null || getBridge().getWebView() == null) return;
+                String script = "document.documentElement.style.setProperty('--native-bottom-inset','" + cssValue + "')";
+                getBridge().getWebView().evaluateJavascript(script, null);
+            });
+            return windowInsets;
+        });
+        ViewCompat.requestApplyInsets(webView);
     }
 }
